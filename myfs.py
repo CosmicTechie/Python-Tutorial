a0c30785fefbbef2ab8aae4de4f9bd5839ea27e8
CosmicTechie <62173480+CosmicTechie@users.noreply.github.com>
2020-10-22 08:21:04 +0530

    Add files via upload

---
+++ myfs.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    '''Output the Sum of two numbers.'''
+    print("Sum is:",x+y)
+def sub(x,y):
+    print("Difference is :",x-y)
+def div(x,y):
+    print("Division is : ", x/y)
+def mul(x,y):
+    print("Multiplication is: ",x*y)
